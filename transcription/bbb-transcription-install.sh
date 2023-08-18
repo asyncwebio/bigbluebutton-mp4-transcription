7fb86818c6c86044c0be6a203ef43e074435fc78
arun-hel <arun@higheredlab.com>
2023-08-18 11:54:37 +0000

    Fix docker files

--- transcription/bbb-transcription-install.sh
+++ transcription/bbb-transcription-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo cp -r bbb_transcription.rb /usr/local/bigbluebutton/core/scripts/post_publish/bbb_transcription.rb
-cd transcription_node_app && npm install && cd ..
-sudo cp -r transcription_node_app /usr/local/bigbluebutton/core/scripts/post_publish/
 sudo mkdir -p /var/bigbluebutton/transcripts
 sudo chown -R bigbluebutton:bigbluebutton /var/bigbluebutton/transcripts
 cat transcription.nginx | sudo tee /etc/bigbluebutton/nginx/bbb-transcription.nginx
